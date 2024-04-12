cf0f33a11cadd0a9955c572b3a0b37db690b3225
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-04-12 01:12:33 -0500

    feat: DB seed

---
+++ seed.js
@@ -0,0 +1,36 @@
+import { csvParse, csvStringify } from './deps.js'
+
+async function fetchPokeData(url) {
+  const resp = await fetch(url)
+  const text = await resp.text()
+  const data = JSON.parse(text)
+  const pokeData = {
+    order: data.order,
+    name: data.name,
+    weight: data.weight,
+    height: data.height,
+    types: data.types[0].type.name,
+    sprite: data.sprites["front_default"],
+    cries: data.cries.latest,
+  }
+  return pokeData
+}
+
+let pokelist = await fetch("https://pokeapi.co/api/v2/pokemon?limit=251&offset=0")
+pokelist = await pokelist.text()
+pokelist = JSON.parse(pokelist).results
+
+let pokemap = await Promise.all(pokelist.map(poke => fetchPokeData(poke.url)))
+
+let pokeCSV = csvStringify(pokemap, {
+  columns: [
+    'order',
+    'name',
+    'weight',
+    'height',
+    'types',
+  ]
+})
+
+await Deno.writeTextFile("db.csv", pokeCSV)
+
