9cf3e8169c317d900beb0c6d96339d95d2211cc6
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-04-14 15:40:50 -0500

    feat: add file-server

--- deps.js
+++ deps.js
@@ -1,4 +1,6 @@
 // CSV
 export { parse as csvParse, stringify as csvStringify } from "https://deno.land/std@0.207.0/csv/mod.ts";
 
+// File Server
+export { serveDir as srvDir, serveFile as srvFl } from "https://deno.land/std@0.207.0/http/file_server.ts";
 
