2cd45c48e8775829015a09f6f25ca961dd3c09ce
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-04-15 23:27:14 -0500

    feat: htmx.js

--- mod.js
+++ mod.js
@@ -1,9 +1,22 @@
-// import { srvFl } from './deps.js'
+import { srvDir } from './deps.js'
 import { router } from './routes/index.js'
 
 const port = 8000
 
 async function reqHandler(req, param) {
+  const url = new URL(req.url)
+
+  console.log(url.pathname)
+
+  if (url.pathname.startsWith("/public")) {
+    return srvDir(req, {
+      fsRoot: "public",
+      urlRoot: "public",
+      showDirListing: true,
+      showIndex: false
+    });
+  }
+
   return await router.route(req);
 }
 
