bae3a73c78750cfffbeccc2a8c1ba6b60a5888c9
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-04-16 00:31:44 -0500

    ci: deno config

--- deps.js
+++ deps.js
@@ -1,6 +1,6 @@
 // CSV
-export { parse as csvParse, stringify as csvStringify } from "https://deno.land/std@0.207.0/csv/mod.ts";
+export { parse as csvParse, stringify as csvStringify } from "csv"
 
 // File Server
-export { serveDir as srvDir, serveFile as srvFl } from "https://deno.land/std@0.207.0/http/file_server.ts";
+export { serveDir as srvDir, serveFile as srvFl } from "flSrv"
 
