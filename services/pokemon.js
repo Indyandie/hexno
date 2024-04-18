a168fe74c927c9962169c3093b00d01521d3fea6
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-04-18 00:28:06 -0500

    feat: htmx - get pokemon article

--- services/pokemon.js
+++ services/pokemon.js
@@ -14,14 +14,18 @@ export const getPokemon = async (id) => {
 
 export const htmxAllPokemon = async () => {
   const pokemon = await getAllPokemon()
-  const htmx = pokemon.map((poke) => {
-    return `<li id="pokemon-${poke.id}"><header>${poke.name}</header><img src="${poke.sprite} alt="${poke.name}"  /><p><ul><li>weight: ${poke.weight}</li><li>height: ${poke.height}</li><li>types: ${poke.types}</li></ul></p></li>`
-  })
+  const htmx = pokemon.map((poke) => (`<li id="pokemon-${poke.id}"><header>${poke.name}</header><img src="${poke.sprite}" alt="${poke.name}"  /><p><ul><li>weight: ${poke.weight}</li><li>height: ${poke.height}</li><li>types: ${poke.types}</li></ul></p></li>`))
   htmx.unshift('<ul>')
   htmx.push('<ul>')
-  const pokelist = JSON.stringify(htmx.join(''))
-  // return "<ol>" + pokelist
-  return pokelist.replace(/\\"/g, '"').slice(1, -1)
+
+  const pokelist = JSON.stringify(htmx.join('')).replace(/\\"/g, '"').slice(1, -1)
+  return pokelist
 }
 
+export const htmxPokemon = async (id) => {
+  const pokemon = await getPokemon(id)
+  const htmx = await `<article><h2>${pokemon.name}</h2><img src="${pokemon.sprite}" alt="${pokemon.name}"/><table><tr><th>weight</th><td>${pokemon.weight}</td></tr><tr><th>height</th><td>${pokemon.height}</td></tr><tr><th>type</th><td>${pokemon.type}</td></tr></table></article>`
+
+  return htmx
+}
 
