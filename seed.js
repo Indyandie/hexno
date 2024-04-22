0d64a14049b390ee31f85ed12297f808463a222c
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-04-22 01:17:12 -0500

    docs(jsdoc): define pokemon object and fetchPokeData function

--- seed.js
+++ seed.js
@@ -1,9 +1,25 @@
-import { csvParse, csvStringify } from './deps.js'
+import { csvParse, csvStringify } from "./deps.js";
 
+/**
+ * @typedef {object} Pokemon
+ * @property {number} id
+ * @property {string} name
+ * @property {number} weight
+ * @property {number} height
+ * @property {string} types
+ * @property {string} sprite - URL
+ * @property {string} cries - URL
+ */
+
+/**
+ * Fetch pokemon data and return filter object.
+ * @param {string} url - https://pokeapi.co/api/v2/pokemon/:id
+ * @return {Pokemon} pokemon: id, name, weight, height, types, sprite, cries
+ */
 async function fetchPokeData(url) {
-  const resp = await fetch(url)
-  const text = await resp.text()
-  const data = JSON.parse(text)
+  const resp = await fetch(url);
+  const text = await resp.text();
+  const data = JSON.parse(text);
   const pokeData = {
     id: data.id,
     name: data.name,
@@ -12,28 +28,31 @@ async function fetchPokeData(url) {
     types: data.types[0].type.name,
     sprite: data.sprites["front_default"],
     cries: data.cries.latest,
-  }
-  return pokeData
+  };
+  return pokeData;
 }
 
-let pokelist = await fetch("https://pokeapi.co/api/v2/pokemon?limit=251&offset=0")
-pokelist = await pokelist.text()
-pokelist = JSON.parse(pokelist).results
+let pokelist = await fetch(
+  "https://pokeapi.co/api/v2/pokemon?limit=251&offset=0",
+);
+pokelist = await pokelist.text();
+pokelist = JSON.parse(pokelist).results;
 
-let pokemap = await Promise.all(pokelist.map(poke => fetchPokeData(poke.url)))
+let pokemap = await Promise.all(
+  pokelist.map((poke) => fetchPokeData(poke.url)),
+);
 
 let pokeCSV = csvStringify(pokemap, {
   columns: [
-    'id',
-    'name',
-    'weight',
-    'height',
-    'types',
-    'sprite',
-  ]
-})
+    "id",
+    "name",
+    "weight",
+    "height",
+    "types",
+    "sprite",
+  ],
+});
 
-pokeCSV = pokeCSV.replace(/-/g, "_")
+pokeCSV = pokeCSV.replace(/-/g, "_");
 
-await Deno.writeTextFile("./models/pokemon.csv", pokeCSV)
-
+await Deno.writeTextFile("./models/pokemon.csv", pokeCSV);
