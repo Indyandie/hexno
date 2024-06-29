438b707b9dbbc31fe1f50d04f538e161668fb1e1
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-06-29 01:38:26 -0500

    style: format

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,21 +1,28 @@
-import { getAllPokemon, getPokemon, htmxAllPokemon, htmxPokemon } from '../services/pokemon.js'
+import {
+  filterPokemon,
+  getAllPokemon,
+  getPokemon,
+  htmxAllPokemon,
+  htmxPokemon,
+} from '../services/pokemon.js'
 
 const status404 = new Response(
   null,
   {
     status: 404,
-    statusText: "Not Found",
-  }
+    statusText: 'Not Found',
+  },
 )
 
-export const listPokemon = async () => new Response(
-  JSON.stringify(await getAllPokemon()),
-  {
-    headers: {
-      'content-type': 'text/json; charset=utf-8'
-    }
-  }
-)
+export const listPokemon = async () =>
+  new Response(
+    JSON.stringify(await getAllPokemon()),
+    {
+      headers: {
+        'content-type': 'text/json; charset=utf-8',
+      },
+    },
+  )
 
 export const getPokemonById = async (req, match) => {
   const pokeId = match.pathname.groups.id
@@ -29,21 +36,21 @@ export const getPokemonById = async (req, match) => {
     JSON.stringify(pokeRes),
     {
       headers: {
-        'Content-Type': 'text/json; charset=utf-8'
-      }
-    }
+        'Content-Type': 'text/json; charset=utf-8',
+      },
+    },
   )
 }
 
-export const htmxAllPokemonCtrl = async () => new Response(
-  await htmxAllPokemon(),
-  {
-    headers: {
-      'Content-Type': 'text/html; charset=utf-8'
-    }
-  }
-
-)
+export const htmxAllPokemonCtrl = async () =>
+  new Response(
+    await htmxAllPokemon(),
+    {
+      headers: {
+        'Content-Type': 'text/html; charset=utf-8',
+      },
+    },
+  )
 
 export const htmxPokemonCtrl = async (req, match) => {
   const pokeId = match.pathname.groups.id
@@ -57,9 +64,8 @@ export const htmxPokemonCtrl = async (req, match) => {
     pokeRes,
     {
       headers: {
-        'Content-Type': 'text/html; charset=utf-8'
-      }
-    }
+        'Content-Type': 'text/html; charset=utf-8',
+      },
+    },
   )
 }
-
