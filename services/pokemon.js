a7cf2577d3c3b8bc2d6b62beaae44f74a42a1ad9
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-06-29 20:44:38 -0500

    fix(service): use unique identifiers for ids and function name
    Resolves `SyntaxError: Identifier 'pikachuDialog' has already been
    declared` error.

--- services/pokemon.js
+++ services/pokemon.js
@@ -30,12 +30,13 @@ export const getPokemon = async (id) => {
 
 export const htmxAllPokemon = async (query) => {
   const pokemon = query ? await getAllPokemon(query) : await getAllPokemon()
+  const now = Date.now()
 
   const htmx = pokemon.map((
     poke,
-  ) => (`<li id="pokemon-${poke.id}"><button id="show-dialog-${poke.name}"><figure><img src="${poke.sprite}" alt="${poke.name}" /><figcaption>${poke.name}</figcaption></figure></button><dialog id="${poke.name}Dialog"><article hx-get="/pokemon-htmx/${poke.id}" hx-trigger="intersect"></article><button autofocus>Close</button></dialog><script>const ${poke.name}Dialog = document.querySelector("#${poke.name}Dialog");const ${poke.name}ShowButton = document.querySelector("#show-dialog-${poke.name}");const ${poke.name}CloseButton = document.querySelector("#${poke.name}Dialog button");${poke.name}ShowButton.addEventListener("click", () => {${poke.name}Dialog.showModal();});${poke.name}CloseButton.addEventListener("click", () => {${poke.name}Dialog.close();});</script>`))
-  htmx.unshift('<ul>')
-  htmx.push('<ul>')
+  ) => (`<li id="pokemon-${poke.id}"><button id="show-dialog-${poke.name}"><figure><img src="${poke.sprite}" alt="${poke.name}" /><figcaption>${poke.name}</figcaption></figure></button><dialog id="${poke.name}Dialog${now}"><article hx-get="/pokemon-htmx/${poke.id}" hx-trigger="intersect"></article><button autofocus>Close</button></dialog><script>const ${poke.name}Dialog${now} = document.querySelector("#${poke.name}Dialog${now}");const ${poke.name}${now}ShowButton = document.querySelector("#show-dialog-${poke.name}");const ${poke.name}${now}CloseButton = document.querySelector("#${poke.name}Dialog${now} button");${poke.name}${now}ShowButton.addEventListener("click", () => {${poke.name}Dialog${now}.showModal();});${poke.name}${now}CloseButton.addEventListener("click", () => {${poke.name}Dialog${now}.close();});</script>`))
+  htmx.unshift('<ul id="pokemon-results">')
+  htmx.push('</ul>')
 
   const pokelist = JSON.stringify(htmx.join('')).replace(/\\"/g, '"').slice(
     1,
