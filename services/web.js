92b2cd021e81d5a042964e38f9859663d597b4db
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-07-10 00:28:24 -0500

    feat(service): init web services

---
+++ services/web.js
@@ -0,0 +1,62 @@
+import { listPokemon } from './pokemon.js'
+
+function htmlTemplate(
+  title,
+  body,
+  meta = null,
+  redirect = null,
+  delay = 0,
+) {
+  const html = `<!doctype html>
+<html lang="en" dir="auto">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    ${meta ? meta : ''}
+    ${
+    !redirect
+      ? ''
+      : '<meta http-equiv="Refresh" content="' + delay + ", url='" + redirect +
+        '\'" >'
+  }
+    <title>${title}</title>
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔴</text></svg>" />
+  </head>
+  <body>
+    <header>
+      <h1><a href="/web">Pokemon<a></h1>
+    </header>
+    ${body}
+  </body>
+</html>`
+
+  return html
+}
+
+export async function htmlPageMain(query = false) {
+  const pokemon = query ? await listPokemon(query) : await listPokemon()
+  const now = Date.now()
+
+  const pokeList = pokemon.map(
+    (
+      poke,
+    ) =>
+      `<li id="pokemon-${poke.id}"><a href="/pokemon/${poke.id}" ><figure><img src="${poke.sprite}" alt="${poke.name}" /><figcaption>${poke.name}</figcaption></figure></a>`,
+  )
+  pokeList.unshift('<ul id="pokemon-results">')
+  pokeList.push('</ul>')
+
+  const pokeUL = JSON.stringify(pokeList.join('')).replace(/\\"/g, '"').slice(
+    1,
+    -1,
+  )
+
+  const form = `<form method="GET" action="/web">
+<input type="search" id="q" name="q" value="${query}">
+<button>Search</button>
+</form>`
+  const html = form + pokeUL
+
+  return htmlTemplate('Pokemon', html)
+}
