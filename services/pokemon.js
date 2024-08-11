4da551c83bf52a3da2eba464d2821136e1f312f5
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-08-11 00:19:02 -0500

    chore(service): remove htmlListPokemon()

--- services/pokemon.js
+++ services/pokemon.js
@@ -467,35 +467,6 @@ export const deletePokemon = async (id) => {
 // HTML
 
 /**
- * Get all pokemon an return an unorder list with embedded dialogs.
- * @param {string} [query] - filter pokemon by name
- * @returns {(html|false)} HTML fragment
- */
-export const htmlListPokemon = async (query = false) => {
-  // lsp keeps telling me await has no effect but when I remove it things break...
-  const pokemon = query ? await listPokemon(query) : await listPokemon()
-  const now = Date.now()
-
-  if (query && pokemon < 1) {
-    return `<p>No pokemon results for <b>${query}</b></p>`
-  } else {
-    const html = pokemon.map(
-      (
-        poke,
-      ) =>
-        `<li id="pokemon-${poke.id}"><button id="show-dialog-${poke.name}"><figure><img src="${poke.sprite}" alt="${poke.name}" /><figcaption>${poke.name}</figcaption></figure></button><dialog id="${poke.name}Dialog${now}"><article hx-get="/hx/pokemon/${poke.id}" hx-trigger="intersect"></article><button autofocus>Close</button></dialog><script>const ${poke.name}Dialog${now} = document.querySelector("#${poke.name}Dialog${now}");const ${poke.name}${now}ShowButton = document.querySelector("#show-dialog-${poke.name}");const ${poke.name}${now}CloseButton = document.querySelector("#${poke.name}Dialog${now} button");${poke.name}${now}ShowButton.addEventListener("click", () => {${poke.name}Dialog${now}.showModal();});${poke.name}${now}CloseButton.addEventListener("click", () => {${poke.name}Dialog${now}.close();});</script>`,
-    )
-    html.unshift('<ul id="pokemon-results">')
-    html.push('</ul>')
-
-    return JSON.stringify(html.join('')).replace(/\\"/g, '"').slice(
-      1,
-      -1,
-    )
-  }
-}
-
-/**
  * Get a pokemon an return an article.
  * @param {number} id - pokemon id
  * @returns {(html|false)} HTML fragment
