ed4d9717bc5b914f5cb894eccf1b7342b0889f40
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-10-05 13:54:08 -0500

    style: format

--- mod.js
+++ mod.js
@@ -1,27 +1,27 @@
-import { srvDir } from "./deps.js";
-import { router } from "./routes/index.js";
+import { srvDir } from './deps.js'
+import { router } from './routes/index.js'
 
-const port = 8000;
+const port = 8000
 
 /**
  * @param {Request} req
  * @returns {Promise<Response>}
  */
 async function reqHandler(req) {
-  const url = new URL(req.url);
+  const url = new URL(req.url)
 
-  if (url.pathname.startsWith("/public")) {
+  if (url.pathname.startsWith('/public')) {
     return srvDir(req, {
-      fsRoot: "public",
-      urlRoot: "public",
+      fsRoot: 'public',
+      urlRoot: 'public',
       showDirListing: true,
       showIndex: false,
-    });
+    })
   }
 
-  return await router.route(req);
+  return await router.route(req)
 }
 
-Deno.serve(reqHandler, { port });
+Deno.serve({ port }, reqHandler)
 
-console.log(`Server is running on http://localhost:${port}`);
+console.log(`Server is running on http://localhost:${port}`)
